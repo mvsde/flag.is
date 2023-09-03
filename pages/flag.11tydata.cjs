b4a73f1f5076714f3a0d82c57b7a7424e704f454
Fynn Becker <post@fynn.be>
2023-09-03 12:24:18 +0200

    feat: Make meta description configurable

--- pages/flag.11tydata.cjs
+++ pages/flag.11tydata.cjs
@@ -7,6 +7,7 @@ module.exports = {
 	permalink: (data) => `${data.id}/index.html`,
 	eleventyComputed: {
 		title: (data) => data.flags[data.id].name,
+		description: (data) => `${data.flags[data.id].name} pride flag`,
 		social: {
 			image: (data) => `data/flags/${data.id}.svg`,
 			image_alt: (data) => `${data.flags[data.id].name} flag`,
--- pages/index.11tydata.cjs
+++ pages/index.11tydata.cjs
@@ -1,3 +1,8 @@
 module.exports = {
 	title: "Pride Flags",
+	description: "Filter pride flags based on colors and patterns.",
+	social: {
+		image: "img/default.social.png",
+		image_alt: "Rainbow flag",
+	},
 };
