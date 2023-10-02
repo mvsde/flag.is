a050ab44fe357075af49e1e5aa7bdec353ff205c
Fynn Becker <post@fynn.be>
2023-10-02 10:16:34 +0200

    feat: Make flag description and alt mandatory

--- pages/flag.11tydata.cjs
+++ pages/flag.11tydata.cjs
@@ -9,12 +9,11 @@ module.exports = {
 	permalink: (data) => `${data.id}/index.html`,
 	eleventyComputed: {
 		title: (data) => data.flags[data.id].name,
-		description: (data) =>
-			data.flags[data.id].description ??
-			`${data.flags[data.id].name} pride flag`,
+		description: (data) => data.flags[data.id].description,
 		social: {
 			image: (data) => `public/img/${data.id}.svg`,
-			imageAlt: (data) => `${data.flags[data.id].name} flag`,
+			imageAlt: (data) =>
+				`${data.flags[data.id].name} flag. ${data.flags[data.id].alt}`,
 			imagePreset: "flag",
 		},
 	},
--- pages/index.11tydata.cjs
+++ pages/index.11tydata.cjs
@@ -2,6 +2,7 @@ module.exports = {
 	description: "Filter pride flags based on colors and patterns.",
 	social: {
 		image: "img/default.social.png",
-		imageAlt: "Rainbow flag",
+		imageAlt:
+			"Rainbow flag. 6 horizontal stripes make a rainbow: red, orange, yellow, green, blue, purple.",
 	},
 };
