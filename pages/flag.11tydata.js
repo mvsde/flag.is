197c1fcab8194483ad3733db9ad6212078ea647f
Fynn Ellie Becker <post@fynn.be>
2025-05-25 08:29:31 +0200

    feat(flags): Semi-automatic related flags system

--- pages/flag.11tydata.js
+++ pages/flag.11tydata.js
@@ -1,5 +1,6 @@
 export default {
 	layout: "rich-text.njk",
+	tags: ["flag"],
 	pagination: {
 		data: "flags",
 		size: 1,
@@ -15,6 +16,26 @@ export default {
 			imageAlt: (data) =>
 				`${data.flags[data.id].name} flag. ${data.flags[data.id].alt}`,
 			imagePreset: "flag",
+		},
+		categories: (data) => {
+			const flag = data.flags[data.id];
+
+			if (!Array.isArray(flag.related)) return;
+
+			const collection = data.collections.flag;
+
+			return flag.related.map((tag) => ({
+				name: data.flags[tag].name,
+				flags: collection
+					.filter((item) => data.flags[item.data.id].related?.includes(tag))
+					.filter((item) => data.id !== item.data.id)
+					.map((item) => ({
+						id: item.data.id,
+						name: data.flags[item.data.id].name,
+						url: item.page.url,
+						aspectRatio: data.flags[item.data.id].aspectRatio,
+					})),
+			}));
 		},
 	},
 };
